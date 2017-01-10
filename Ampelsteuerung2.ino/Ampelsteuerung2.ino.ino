7b0847b4fa740e23f52d470f26c2607643d68c0a
Boas Lehrke <boas.lehrke@live.de>
2017-01-10 12:51:13 +0100

    Code angepasst

--- Ampelsteuerung2.ino/Ampelsteuerung2.ino.ino
+++ Ampelsteuerung2.ino/Ampelsteuerung2.ino.ino
@@ -1,47 +1,72 @@
-#define ampel1red1  0
-#define ampel1yellow1 1
-#define ampel1green1 2
-#define ampel1red2  3
-#define ampel1green2 4
-#define ampel2red1 5
-#define ampel2yellow1 6
-#define ampel2green1 7
-#define ampel2red2 8
-#define ampel2green2 9
-#define ampel1Taster 12
-#define ampel2Taster 13
+// Each constant contains the index of PINS
+enum MAP{ AMPEL1RED1=0,
+          AMPEL1YELLOW1=1,
+          AMPEL1GREEN1=2,
+          AMPEL1RED2=3,
+          AMPEL1GREEN2=4,
+          AMPEL2RED1=5,
+          AMPEL2YELLOW1=6,
+          AMPEL2GREEN1=7,
+          AMPEL2RED2=8,
+          AMPEL2GREEN2=9,
+          AMPEL1TASTER=10,
+          AMPEL2TASTER=11
+       };
 
+// Array with the used pinnumbers
+// Which Pin is what constant is in the enum MAP          
+const int PINS[] = {0,1,2,3,4,5,6,7,8,9,12,13};
 
 void setup() {
-  pinMode(ampel1red1, OUTPUT);
-  pinMode(ampel1yellow1, OUTPUT);
-  pinMode(ampel1green1, OUTPUT);
-  pinMode(ampel1red2, OUTPUT);
-  pinMode(ampel1green2, OUTPUT);
-  pinMode(ampel2red1, OUTPUT);
-  pinMode(ampel2yellow1, OUTPUT);
-  pinMode(ampel2green1, OUTPUT);
-  pinMode(ampel2red2, OUTPUT);
-  pinMode(ampel2green2, OUTPUT);
-  pinMode(ampel1Taster, INPUT);
-  pinMode(ampel2Taster, INPUT);
+  pinMode(PINS[AMPEL1RED1], OUTPUT);
+  pinMode(PINS[AMPEL1YELLOW1], OUTPUT);
+  pinMode(PINS[AMPEL1GREEN1], OUTPUT);
+  pinMode(PINS[AMPEL1RED2], OUTPUT);
+  pinMode(PINS[AMPEL1GREEN2], OUTPUT);
+  pinMode(PINS[AMPEL2RED1], OUTPUT);
+  pinMode(PINS[AMPEL2YELLOW1], OUTPUT);
+  pinMode(PINS[AMPEL2GREEN1], OUTPUT);
+  pinMode(PINS[AMPEL2RED2], OUTPUT);
+  pinMode(PINS[AMPEL2GREEN2], OUTPUT);
+  pinMode(PINS[AMPEL1TASTER], INPUT);
+  pinMode(PINS[AMPEL2TASTER], INPUT);
   Serial.begin(9600);
 }
 
-// ampel1Taster = 2^0
-// ampel2Taster = 2^1
-int input(){
-  int t1 = int(digitalRead(ampel1Taster) && HIGH);
-  int t2 = int(digitalRead(ampel2Taster) && HIGH);
-  return int( t1*pow(2,0) + t2*pow(2,1) );
-}
-
-void output(int outVal){
-  
-}
 void loop() {
-  Serial.println(input());
+  int max = sizeof(PINS)/sizeof(PINS[0]);
+  for(int i = 0; i < max;i++){
+    Serial.println(readDPin(i));
+  }
+  delay(500);
 }
 
+// AMPEL1TASTER = 2^0
+// AMPEL2TASTER = 2^1
+int input() {
+  int t1 = int(digitalRead(AMPEL1TASTER) && HIGH);
+  int t2 = int(digitalRead(AMPEL2TASTER) && HIGH);
+  return int( t1 * pow(2, 0) + t2 * pow(2, 1) );
+}
 
+void output(int outVal) {
+  for(int i = 0;i < sizeof(PINS);i++){
+    writeDPin(PINS[i],(outVal && pow(2, PINS[i]) == HIGH) ? HIGH : LOW);
+  }
+}
 
+int readDPin(int k){
+  return digitalRead(k);
+}
+
+void writeDPin(int k, int v){
+  digitalWrite(k,v);
+}
+
+int readMDPin(MAP k){
+  return readDPin(PINS[k]);
+}
+
+void writeMDPin(MAP k, int v){
+  digitalWrite(PINS[k],v);
+}
