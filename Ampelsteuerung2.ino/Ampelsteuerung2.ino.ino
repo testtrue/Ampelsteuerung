75c2e2ec9ebda556ed5bd600f677bec7969522be
Boas Lehrke <boas.lehrke@live.de>
2017-01-19 16:51:00 +0100

    Final Version

--- Ampelsteuerung2.ino/Ampelsteuerung2.ino.ino
+++ Ampelsteuerung2.ino/Ampelsteuerung2.ino.ino
@@ -1,23 +1,30 @@
 // Each constant contains the index of PINS
 enum MAP{ AMPEL1RED1=0,
-          AMPEL1YELLOW1=1,
-          AMPEL1GREEN1=2,
-          AMPEL1RED2=3,
-          AMPEL1GREEN2=4,
-          AMPEL2RED1=5,
-          AMPEL2YELLOW1=6,
-          AMPEL2GREEN1=7,
-          AMPEL2RED2=8,
-          AMPEL2GREEN2=9,
-          AMPEL1TASTER=10,
-          AMPEL1TASTERRESET=11,
-          AMPEL2TASTER=12,
-          AMPEL2TASTERRESET=13
+          AMPEL1YELLOW1,
+          AMPEL1GREEN1,
+          AMPEL1RED2,
+          AMPEL1GREEN2,
+          AMPEL2RED1,
+          AMPEL2YELLOW1,
+          AMPEL2GREEN1,
+          AMPEL2RED2,
+          AMPEL2GREEN2,
+          AMPEL1TASTERRESET,
+          AMPEL2TASTERRESET
        };
+
+enum AMAP{ AMPEL1TASTER,
+           AMPEL2TASTER,
+           LICHTWIEDERSTAND};
+
+// Source: https://www.cs.tut.fi/~jkorpela/round.html
+#define round(x) ((x)>=0?(long)((x)+0.5):(long)((x)-0.5))
 
 // Array with the used pinnumbers
 // Which Pin is what constant is in the enum MAP          
-const int PINS[] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13};
+const int PINS[] = {2,3,4,5,6,7,8,9,10,11,12,13};
+const int APINS[] = {A1,A2,A3};
+boolean stp0 = true,stp1 = false,stp2 = false,stp3 = false,stp4 = false,stp5 = false,stp6 = false,stp7 = false;
 
 void setup() {
   pinMode(PINS[AMPEL1RED1], OUTPUT);
@@ -31,38 +38,40 @@ void setup() {
   pinMode(PINS[AMPEL2RED2], OUTPUT);
   pinMode(PINS[AMPEL2GREEN2], OUTPUT);
   pinMode(PINS[AMPEL1TASTERRESET],OUTPUT);
-  pinMode(PINS[AMPEL1TASTER], INPUT);
   pinMode(PINS[AMPEL2TASTERRESET],OUTPUT);
-  pinMode(PINS[AMPEL2TASTER], INPUT);
 
+  analogReference(DEFAULT);
+  Serial.begin(115200);
+ 
   int outputval = 0;
   outputval += getAddValue(true,AMPEL1GREEN1);
   outputval += getAddValue(true,AMPEL1RED2);
   outputval += getAddValue(true,AMPEL2RED1);
   outputval += getAddValue(true,AMPEL2RED2);
   output(outputval);
-  
-  Serial.begin(9600);
 }
 
 void loop() {
-  boolean nightmode = false;
   int input = getInput();
-  int ret = 0;
-  if(nightmode){
-    ret = runnightmode(input);
+
+  boolean nightmode = constrain(analogRead(A3),35,100) <= 35;
+  int outputval = 0;
+  if(!nightmode){
+    outputval = standardmode(input);
   }else{
-    ret = runstandardmode(input);
+    outputval = runnightmode(input);
   }
-  output(ret);
+  output(outputval);
+
+  if(mask(outputval,AMPEL1GREEN1) || mask(outputval,AMPEL2GREEN1) ) delay(5000);
+  else if(mask(outputval,AMPEL1YELLOW1) || mask(outputval,AMPEL2YELLOW1)) delay(1000);
+  else if(mask(outputval,AMPEL1RED1) || mask(outputval,AMPEL2RED1)) delay(2000);
   delay(500);
 }
 
-int runstandardmode(int input){
-  boolean stp0 = true,stp1 = false,stp2 = false,stp3 = false,stp4 = false,stp5 = false,stp6 = false,stp7 = false;
-
-  boolean taster1 = mask(input,AMPEL1TASTER);
-  boolean taster2 = mask(input,AMPEL2TASTER);
+int standardmode(int input){
+  boolean taster1 = map(analogRead(APINS[AMPEL1TASTER]),0,500,0,1) == 1;
+  boolean taster2 = map(analogRead(APINS[AMPEL2TASTER]),0,500,0,1) == 1;
   
   if(stp7 && mask(input,AMPEL1YELLOW1) &&
             mask(input,AMPEL1RED1) &&
@@ -70,7 +79,7 @@ int runstandardmode(int input){
     stp7 = false;
     stp0 = true;
   }
-  
+
   if(stp0 && mask(input, AMPEL1GREEN1) &&
             mask(input, AMPEL2RED1) ){
     stp0 = false;
@@ -125,10 +134,10 @@ int runstandardmode(int input){
   outputval += getAddValue(!stp4&&!stp5,AMPEL2RED1);
   outputval += getAddValue(stp4&&taster2,AMPEL2GREEN2);
   outputval += getAddValue(!(stp4&&taster2),AMPEL2RED2);
-
+  
   if(!(stp0 && taster1)) resetFF(AMPEL1TASTERRESET);
   if(!(stp4 && taster2)) resetFF(AMPEL2TASTERRESET);
-  //output(outputval);
+//  Serial.println(outputval);
   return outputval;
 }
 
@@ -136,50 +145,48 @@ int runnightmode(int input){
   return getAddValue(!mask(input,AMPEL2YELLOW1),AMPEL2YELLOW1);
 }
 
+
 int getInput() {
   int ret = 0;
-  for(int i = 0; i < arrayLength(PINS);i++){
-    ret += readDPin(PINS[i]) * pow(2,PINS[i]);
+  int max = sizeof(PINS)/sizeof(PINS[0]);
+  for(int i = 0; i < max;i++){
+    ret += digitalRead(PINS[i]) * round(pow(2,PINS[i]));
   }
   return ret;
 }
 
 void resetFF(MAP m){
-  writeMDPin(m,HIGH);
-  writeMDPin(m,LOW);
+  digitalWrite(PINS[m],HIGH);
+  digitalWrite(PINS[m],LOW);
 }
 
 void output(int outVal) {
-  int max = arrayLength(PINS);
+  int max = sizeof(PINS)/sizeof(PINS[0]);
   for(int i = 0;i < max;i++){
-    writeDPin(PINS[i],mask(outVal,PINS[i]) ? HIGH : LOW);
+    boolean b = mask(outVal,PINS[i]);
+    int p = PINS[i];
+    digitalWrite(p,b ? HIGH : LOW);
   }
 }
 
 /*
- * Berechne arraylänge
- */
-int arrayLength(int a[]){
-  return sizeof(a)/sizeof(a[0]);
-}
-
-/*
- * Es wird der Pin des gemappten Wert m in input ausmaskiert und dann geprüft ob der Wert true ist
+ * Es wird der Pin des gemappten Wert m in input ausmaskiert und dann geprueft ob der Wert true ist
  */ 
 boolean mask(int input, MAP m){
   return mask(input,getMPin(m));
 }
 
 boolean mask(int input, int p){
-  return (input & int(pow(2, p))) == true;
+  int v = round(pow(2, p));
+  return (input & v) == v;
 }
 
 /* 
- *  Gibt den zu addieren Wert zurück wenn b true ist sonst 0
+ *  Gibt den zu addieren Wert zurueck wenn b true ist sonst 0
  */
 int getAddValue(boolean b, MAP m){
   if(b){
-    return pow(2,getMPin(m));
+    return round(pow(2,getMPin(m)));
   }else{
     return 0;
   }
@@ -188,27 +195,4 @@ int getAddValue(boolean b, MAP m){
 // Pinnummer des gemappten Wertes
 int getMPin(MAP m){
   return PINS[m];
-}
-
-// k = Pinnummer
-int readDPin(int k){
-  return digitalRead(k);
-}
-
-/*  k = Pinnummer
- *  v = 1 oder 0
- */ 
-void writeDPin(int k, int v){
-  digitalWrite(k,v);
-}
-
-// k = Wert aus MAP
-int readMDPin(MAP k){
-  return readDPin(PINS[k]);
-}
-/*  k = Wert aus MAP
- *  v = 1 oder 0
- */
-void writeMDPin(MAP k, int v){
-  digitalWrite(PINS[k],v);
 }
